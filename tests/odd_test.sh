80165d4f8b35aa0fb12bd48cafde6915029e159f
jaigret <julien.aigret@student.uclouvain.be>
2021-03-15 23:53:31 +0100

    odd testing

--- tests/odd_test.sh
+++ tests/odd_test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#cd .. &
+
 # cleanup d'un test précédent
 rm -f received_file input_file
 
@@ -7,15 +7,19 @@ rm -f received_file input_file
 dd if=/dev/urandom of=input_file bs=1 count=512 &> /dev/null
 
 # On lance le simulateur de lien avec 10% de pertes et un délais de 50ms
-../link_sim -p 1341 -P 2456 -l 10 -d 50 -R  &> link.log &
+#../link_sim -p 1341 -P 2456 -l 10 -d 50 -R  &> link.log &
+../link_sim -p 1341 -P 2456 -R  &> link.log &
 link_pid=$!
+echo "linksim launched"
 
 # On lance le receiver et capture sa sortie standard
-../receiver -f received_file :: 2456  2> receiver.log &
+../src/receiver -f received_file :: 2456  2> receiver.log &
 receiver_pid=$!
+echo "receiver launched"
 
 cleanup()
 {
+    echo cleanup
     kill -9 $receiver_pid
     kill -9 $link_pid
     exit 0
@@ -23,12 +27,13 @@ cleanup()
 trap cleanup SIGINT  # Kill les process en arrière plan en cas de ^-C
 
 # On démarre le transfert
-if ! ../sender ::1 1341 < input_file 2> sender.log ; then
+if ! ../src/sender ::1 1341 < input_file 2> sender.log ; then
   echo "Crash du sender!"
   cat sender.log
   err=1  # On enregistre l'erreur
 fi
 
+echo "sleeping ..."
 sleep 5 # On attend 5 seconde que le receiver finisse
 
 if kill -0 $receiver_pid &> /dev/null ; then
