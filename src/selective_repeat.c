578b162e8e6fe00b18b2975a32e3d9b4369424f0
mounzera <amine.mounzer@student.uclouvain.be>
2021-03-18 11:01:07 +0100

    rtt mean for timers

--- src/selective_repeat.c
+++ src/selective_repeat.c
@@ -24,22 +24,25 @@ window_receiver_t* init_receiver_window(){
   receive_window->next_seqnum = 0;
   receive_window->window_val = MAX_WINDOW_SIZE;
   receive_window->window_size = MAX_WINDOW_SIZE;
-  for(int i=0; i<31; i++) {
+  for(int i=0; i<MAX_WINDOW_SIZE; i++) {
     receive_window->window[i] = NULL;
   }
   return receive_window;
 }
 
 int set_min_max_rtt(time_t new_time){
-    min_rtt = (new_time < min_rtt) ? new_time : min_rtt;
-    max_rtt = (new_time > max_rtt) ? new_time : max_rtt;
+    time_t actual_new_time = clock() - new_time;
+    min_rtt = (actual_new_time < min_rtt) ? actual_new_time : min_rtt;
+    max_rtt = (actual_new_time > max_rtt) ? actual_new_time : max_rtt;
 }
 
 time_t get_rtt_mean(){
     if(count_rtt < 15){return -1;}
     time_t sum = 0;
+    
     for (size_t i = 0; i < 15; i++)
     {
+        //printf("first: %ld\n", last_rtt[i]);
         sum += last_rtt[i];
     }
     return sum/15;
@@ -62,6 +65,8 @@ int check_ack(window_sender_t* window, int index, int item_window_nb){
         //printf("mod: %d\n", index%32);
         pkt_del(window->window[index%MAX_WINDOW_SIZE]);
         set_min_max_rtt(window->start_time[index%MAX_WINDOW_SIZE]);
+        last_rtt[count_rtt%15] = clock() - window->start_time[index%MAX_WINDOW_SIZE];
+        count_rtt++;
         window->start_time[index%MAX_WINDOW_SIZE] = 0;
         window->window[index%MAX_WINDOW_SIZE] = NULL;
         index--;
@@ -85,7 +90,10 @@ int check_timer(window_sender_t* window, int sock, struct sockaddr_in6 peer_addr
         pkt_t* pkt = window->window[i];
         if(pkt == NULL){continue;}
         //printf("%ld, %ld\n", i, clock() - window->start_time[i]);
-        if(clock() - window->start_time[i] > 500){
+        time_t max_time = get_rtt_mean();
+        if(max_time == -1 || max_time < 100){max_time = 300;}
+        //printf("%ld\n", max_time);
+        if(clock() - window->start_time[i] > max_time+0.1*max_time){
             //printf("add timer: %d\n", pkt_get_seqnum(pkt));
             window->start_time[i] = clock();
             send_message(sock, pkt, peer_addr);
