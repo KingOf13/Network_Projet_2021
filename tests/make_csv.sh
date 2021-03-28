6168dd448afbbc5dd4589ee2b1ed10e8b8b1bbac
Julien Aigret <julien.aigret@student.uclouvain.be>
2021-03-29 01:32:24 +0200

    CSV upload

--- tests/make_csv.sh
+++ tests/make_csv.sh
@@ -1,10 +1,10 @@
 simple="0 0 0 0 0"
-delay="0 200 0 0 0"
-jitter="0 200 50 0 0"
+delay="0 50 0 0 0"
+jitter="0 50 10 0 0"
 loss="10 0 0 0 0"
 cut="0 0 0 10 0"
 error="0 0 0 0 10"
-complete="10 200 50 10 10"
+complete="10 50 10 10 10"
 
 rm -f ../datas/simple.csv ../datas/delayed.csv ../datas/jittered.csv ../datas/loss.csv ../datas/cut.csv ../datas/error.csv ../datas/complete.csv
 echo "; "Simple"; "Delayed"; "Jittered"; "With loss"; "Cut"; "Error"; "Complete"" > ../datas/full_datas.csv
@@ -19,14 +19,16 @@ for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27
   fi
   echo "$i; $simple_time" >> ../datas/simple.csv
   killall -q -9 link_sim
-  delayed_time="$(TIMEFORMAT='%lU'; time ( ./param_test.sh $delay >/dev/null) 2>&1)"
+  echo "Simple transfert n°$i done"
+delayed_time="$(TIMEFORMAT='%lU'; time ( ./param_test.sh $delay >/dev/null) 2>&1)"
   ret=$?
   if [ ret == 1 ]; then
-    echo "Test n°$i failed(delay)"
+    echo "Test n°$i failed (delay)"
     delayed_time=Fail
   fi
   echo "$i; $delayed_time" >> ../datas/delayed.csv
-  killall -q -9 link_sim
+  killall -q -9 link_sim  
+  echo "Delayed transfert n°$i done"
   jittered_time="$(TIMEFORMAT='%lU'; time ( ./param_test.sh $jitter >/dev/null) 2>&1)"
   ret=$?
   if [ ret == 1 ]; then
@@ -34,7 +36,8 @@ for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27
     jittered_time=Fail
   fi
   echo "$i; $jittered_time" >> ../datas/jittered.csv
-  killall -q -9 link_sim
+  killall -q -9 link_sim  
+  echo "Jittered transfert n°$i done"
   lost_time="$(TIMEFORMAT='%lU'; time ( ./param_test.sh $loss >/dev/null) 2>&1)"
   ret=$?
   if [ ret == 1 ]; then
@@ -42,7 +45,8 @@ for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27
     lost_time=Fail
   fi
   echo "$i; $lost_time" >> ../datas/loss.csv
-  killall -q -9 link_sim
+  killall -q -9 link_sim  
+  echo "Transfert with loss n°$i done"
   cut_time="$(TIMEFORMAT='%lU'; time ( ./param_test.sh $cut >/dev/null) 2>&1)"
   ret=$?
   if [ ret == 1 ]; then
@@ -50,7 +54,8 @@ for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27
     cut_time=Fail
   fi
   echo "$i; $cut_time" >> ../datas/cut.csv
-  killall -q -9 link_sim
+  killall -q -9 link_sim  
+  echo "Cut transfert n°$i done"
   errored_time="$(TIMEFORMAT='%lU'; time ( ./param_test.sh $error >/dev/null) 2>&1)"
   ret=$?
   if [ ret == 1 ]; then
@@ -58,15 +63,18 @@ for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27
     errored_time=Fail
   fi
   echo "$i; $errored_time" >> ../datas/error.csv
-  killall -q -9 link_sim
-  complete_time="$(TIMEFORMAT='%lU'; time ( ./param_test.sh $complete >/dev/null) 2>&1)"
-  ret=$?
-  if [ ret == 1 ]; then
-    echo "Test n°$i failed (complete)"
-    complete_time=Fail
-  fi
-  echo "$i; $complete_time" >> ../datas/complete.csv
-  killall -q -9 link_sim
+  killall -q -9 link_sim  
+  echo "Corrupted transfert n°$i done"
+  #complete_time="$(TIMEFORMAT='%lU'; time ( ./param_test.sh $complete >/dev/null) 2>&1)"
+  complete_time="Not tested"
+  #ret=$?
+  #if [ ret == 1 ]; then
+  #  echo "Test n°$i failed (complete)"
+  #  complete_time=Fail
+  #fi
+  #echo "$i; $complete_time" >> ../datas/complete.csv
+  #killall -q -9 link_sim  
+  #echo "Complete transfert n°$i done"
   echo "$i; $simple_time; $delayed_time; $jittered_time; $lost_time; $cut_time; $errored_time; $complete_time" >> ../datas/full_datas.csv
 done
 echo "THE CSV ARE IN DATAS/ "
